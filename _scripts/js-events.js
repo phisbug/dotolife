d4cdc6b80f589782e6b4df409b7b20045de1a644
phisbug <dddexadgat@yahoo.com>
2014-10-29 14:43:41 +0800

    Tested sprite image
    ANIMATING

---
+++ _scripts/js-events.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+document.getElementById('grid').addEventListener('click', function(e)
+{
+	
+	console.log('ul was clicked ');
+	
+}, false);
+
+
+
+document.getElementsByTagName('div').addEventListener('click', function()
+{
+	
+	console.log('div was clicked');
+	
+}, false);
+
